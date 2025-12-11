17e9f266255d1a0a54b5a3d3ce2c3b307fbb8682
Peter <ptqmtl117@gmail.com>
2025-12-10 22:07:43 -0500

    Modify the test
    Modify the test

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,24 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_EQUAL_STRING_MESSAGE("peterzee-tsien", my_username(), "my_username() failed to match the implementation");
+    /**
+     * Step 1: Get the hardcoded username
+     */
+    const char *hardcoded_name = my_username();
+
+    /**
+     * Step 2: Obtain the value from the config file using the helper function
+     */
+    char *conf_name = malloc_username_from_conf_file();
+
+    /**
+     * Step 3: Verify the two strings are equal
+     */
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_name, conf_name, "my_username() does not match the content of conf/username.txt");
+
+    /**
+     * cleanup: The function name 'malloc_...' implies it allocated memory.
+     * In C, we should free this to avoid memory leaks.
+     */
+    free(conf_name);
 }
